ee7dfda3ac0d19416f74a5fc4f48c51cf5d064b3
Matt Newbold <matt.newbold208@gmail.com>
2021-01-25 20:31:59 -0600

    studio BB

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,8 +10,28 @@ public class BalancedBracketsTest {
     //TODO: add tests here
     @Test
     public void emptyTest() {
-        assertEquals(true, true);
+        assertEquals(true, BalancedBrackets.hasBalancedBrackets("[]"));
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+     @Test
+    public void bracketsWord() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+     }
+
+     @Test
+    public void emptyString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+     }
+
+     @Test
+    public void doubleBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+     }
 
 
 }
